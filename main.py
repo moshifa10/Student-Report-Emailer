0a9810830a30e3b8774593689dca9855d23461de
Njabulo Moshifa <njmosjhb025@wstudent.wethinkcode.co.za>
2026-02-10 11:34:24 +0200

    added the txt files

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import pandas
+import pandas as pd
 import smtplib
+import os
 from dotenv import load_dotenv
 
 
-# I will be implementing the student report emailer between teachers and students
-
+# I will be implementing the student report emailer between teachers and studentss
 load_dotenv()
+
+DATA_PATH =  "./data/students.csv"
+data = pd.read_csv(DATA_PATH)
+
+# Get data from the env file
+teacher_email = os.getenv(key="TEACHER_EMAIl")
+password = os.getenv(key="PASSWORD")
+
+# print(teacher_email)
+# m =  data.name.to_list()
+# print(m)
+get_data = {(info["name"],info.email):(info.math, info.science, info.english ) for (index,info)  in data.iterrows()}
+
+
+
+print(get_data)
